5ee470c516645c538ab5a32963bed5213bfd3104
RubeshR143681 <130778523+RubeshR143681@users.noreply.github.com>
2024-09-18 09:25:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { TrafficLightProvider } from "./context/TrafficLightContext";
 const App = () => {
   return (
     <TrafficLightProvider>
-      <div className="App">
+      <div className="App min-h-screen">
         <TrafficLight />
         <ToastContainer />
       </div>
